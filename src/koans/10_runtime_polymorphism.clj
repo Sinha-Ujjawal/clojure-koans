8e678a1767b3ead68ed99caaf75006c3e40ac522
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2022-10-04 22:30:09 +0530

    My submissions

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -1,5 +1,5 @@
 (ns koans.10-runtime-polymorphism
-  (:require [koan-engine.core :refer :all]))
+  (:require [koan-engine.core :refer [meditations]]))
 
 (defn hello
   ([] "Hello World!")
@@ -9,34 +9,36 @@
                           (interpose ", " (cons a more)))
                    "!")))
 
+(defn eats [who what] (str who " " "eats " what "."))
+
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :herbivore [a] (eats (:name a) "veggies"))
+(defmethod diet :carnivore [a] (eats (:name a) "animals"))
+(defmethod diet :default [a] (str "I don't know what " (:name a) " eats."))
 
 (meditations
-  "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+ "Some functions can be used in different ways - with no arguments"
+ (= "Hello World!" (hello))
 
-  "With one argument"
-  (= __ (hello "world"))
+ "With one argument"
+ (= "Hello, you silly world." (hello "world"))
 
-  "Or with many arguments"
-  (= __
-     (hello "Peter" "Paul" "Mary"))
+ "Or with many arguments"
+ (= "Hello to this group: Peter, Paul, Mary!"
+    (hello "Peter" "Paul" "Mary"))
 
-  "Multimethods allow more complex dispatching"
-  (= "Bambi eats veggies."
-     (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
+ "Multimethods allow more complex dispatching"
+ (= "Bambi eats veggies."
+    (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
 
-  "Animals have different names"
-  (= "Thumper eats veggies."
-     (diet {:species "rabbit" :name "Thumper" :age 1 :eater :herbivore}))
+ "Animals have different names"
+ (= "Thumper eats veggies."
+    (diet {:species "rabbit" :name "Thumper" :age 1 :eater :herbivore}))
 
-  "Different methods are used depending on the dispatch function result"
-  (= "Simba eats animals."
-     (diet {:species "lion" :name "Simba" :age 1 :eater :carnivore}))
+ "Different methods are used depending on the dispatch function result"
+ (= "Simba eats animals."
+    (diet {:species "lion" :name "Simba" :age 1 :eater :carnivore}))
 
-  "You may use a default method when no others match"
-  (= "I don't know what Rich Hickey eats."
-     (diet {:name "Rich Hickey"})))
+ "You may use a default method when no others match"
+ (= "I don't know what Rich Hickey eats."
+    (diet {:name "Rich Hickey"})))
